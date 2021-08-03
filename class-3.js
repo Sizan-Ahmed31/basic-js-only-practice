e8e4036525fca51e6e42b596d075aa7d3e80c626
Sizan-Ahmed <asizan631@gmail.com>
2021-08-03 20:13:12 +0600

    some change

---
+++ class-3.js
@@ -0,0 +1,8 @@
+const productName = "potato";
+const productPrice = 30;
+const quantity = 3;
+const available = true;
+
+// const , let , var we should use this keyword to write a variable. when we write a variable we should avoid some mistake like we should not space between the name, at the start point we should not use any number but we can use $ or _ . but the standard way to write variable name we can use camelcasing like productName. 
+
+console.log("product- " + productName, "price- " + productPrice, "quantity-" + quantity, "available- " + available)
