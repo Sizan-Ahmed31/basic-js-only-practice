8d2e4dbba3476bde79dda95d9e2cccd7eeca3aa0
Sizan-Ahmed <asizan631@gmail.com>
2021-08-04 06:44:16 +0600

    adding a file

---
+++ class-4.js
@@ -0,0 +1,15 @@
+// here we will learn 2 kinds of data type in javascript:
+// primitives data type defiend a single value
+// primitives: 1.string 
+// 2.number    
+// 3.boolean
+// 4.undefined
+// 5.null
+// 6.symbol
+// 7.bigint
+
+
+// primitives data type defiend complex value
+// object: 1.object
+//2.array
+//3.function
