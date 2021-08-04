a5080eb754c1000075072aca9058373b35eb6629
Sizan-Ahmed <asizan631@gmail.com>
2021-08-04 07:41:05 +0600

    adding js 5 no file

--- class-5.js
+++ class-5.js
@@ -1,4 +1,11 @@
  const myName = 'sizan'
  const age = 20
  const job = 'student'
- console.log(`name-${myName} age-${age} job-${job}`)
+ console.log(`name-${myName} age-${age} job-${job}`);
+
+ const productName = "potato";
+ const productPrice = 30;
+ const quantity = 3;
+ const available = true;
+
+ console.log(`product-${productName} price-${productPrice} quantity-${quantity} available-${available}`);
